7ad86564bbe5b034848c17b29351d2db41cc8389
Tilwani <tilwanit@amazon.com>
2018-12-08 16:22:49 -0800

    initial basic load

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import _ from "lodash";
+import "./assets/app.css";
+import { dogs } from "./assets/data/dogs.json";
+
+function component() {
+  let cardNodes = dogs.map(dog => {
+
+    let img = document.createElement("img");
+    img.src =  `.${dog.thumbnail}`;
+
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add('img-container');
+    imgContainer.appendChild(img);
+
+    let h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode(dog.title));
+
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(dog.description));
+
+    let figcaption = document.createElement("figcaption");
+    figcaption.appendChild(h2);
+    figcaption.appendChild(p);
+
+    let figure = document.createElement("figure");
+    figure.appendChild(imgContainer);
+    figure.appendChild(figcaption);
+    figure.classList.add("card");
+
+    return figure;
+  });
+
+  return _.reduce(
+    cardNodes,
+    (parent, child) => {
+      parent.appendChild(child);
+      return parent;
+    },
+    document.createElement("main")
+  );
+}
+
+document.body.appendChild(component());
