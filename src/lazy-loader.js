d5495b0b4fcbc95f735651cb498e8b3ad76b28d2
Tilwani <tilwanit@amazon.com>
2018-12-09 21:53:14 -0800

    lazy loader var to const

--- src/lazy-loader.js
+++ src/lazy-loader.js
@@ -1,11 +1,8 @@
 // https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
 export default function() {
-  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  const lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
   if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function(
-      entries,
-      observer
-    ) {
+    const lazyImageObserver = new IntersectionObserver(function(entries) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
           let lazyImage = entry.target;
