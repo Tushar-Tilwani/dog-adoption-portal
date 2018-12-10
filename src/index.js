e11a21df914597b325c59f32d85aa92192b3465a
Tilwani <tilwanit@amazon.com>
2018-12-09 20:38:37 -0800

    Add infite scroll logic

--- src/index.js
+++ src/index.js
@@ -3,51 +3,62 @@ import "./assets/app.css";
 import { dogs } from "./assets/data/dogs.json";
 
 const PAGE_CONFIGS = {
-  pageLength: 5,
-  pageOffset: 1000
+  pageLength: 6,
+  pageOffset: 1000,
+  mainDiv: document.getElementById("main-content")
+};
+const state = {
+  currentPage: 0
 };
 
-function component() {
-  let cardNodes = dogs.map(dog => {
-    let img = document.createElement("img");
-    img.src = `.${dog.thumbnail}`;
+function getFigure(dog) {
+  let img = document.createElement("img");
+  img.src = `.${dog.thumbnail}`;
 
-    let imgContainer = document.createElement("div");
-    imgContainer.classList.add("img-container");
-    imgContainer.appendChild(img);
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  imgContainer.appendChild(img);
 
-    let h2 = document.createElement("h2");
-    h2.appendChild(document.createTextNode(dog.title));
+  let h2 = document.createElement("h2");
+  h2.appendChild(document.createTextNode(dog.title));
 
-    let p = document.createElement("p");
-    p.appendChild(document.createTextNode(dog.description));
+  let p = document.createElement("p");
+  p.appendChild(document.createTextNode(dog.description));
 
-    let a = document.createElement("a");
-    a.appendChild(document.createTextNode(`Adopt ${dog.title}`));
-    a.classList.add("adopt-link");
-    a.href = `./adopt.html?id=${dog.id}`;
+  let a = document.createElement("a");
+  a.appendChild(document.createTextNode(`Adopt ${dog.title}`));
+  a.classList.add("adopt-link");
+  a.href = `./adopt.html?id=${dog.id}`;
 
-    let figcaption = document.createElement("figcaption");
-    figcaption.appendChild(h2);
-    figcaption.appendChild(p);
+  let figcaption = document.createElement("figcaption");
+  figcaption.appendChild(h2);
+  figcaption.appendChild(p);
 
-    let figure = document.createElement("figure");
-    figure.appendChild(imgContainer);
-    figure.appendChild(figcaption);
-    figure.appendChild(a);
-    figure.classList.add("card");
+  let figure = document.createElement("figure");
+  figure.appendChild(imgContainer);
+  figure.appendChild(figcaption);
+  figure.appendChild(a);
+  figure.classList.add("card");
 
-    return figure;
-  });
+  return figure;
+}
 
-  return _.reduce(
+function component(pageNum) {
+  const start = pageNum * PAGE_CONFIGS.pageLength;
+  const end = (pageNum + 1) * PAGE_CONFIGS.pageLength;
+
+  let cardNodes = dogs.slice(start, end).map(dog => getFigure(dog));
+
+  const fragment = _.reduce(
     cardNodes,
     (parent, child) => {
       parent.appendChild(child);
       return parent;
     },
-    document.createElement("main")
+    document.createDocumentFragment()
   );
+
+  PAGE_CONFIGS.mainDiv.appendChild(fragment);
 }
 
 function getPageTop() {
@@ -72,13 +83,22 @@ function getPageHeight() {
   );
 }
 
+function isMoreDataAvilable() {
+  return state.currentPage * PAGE_CONFIGS.pageLength < dogs.length;
+}
+
 function infiteScroll(e) {
-  console.log(getPageHeight() , getPageTop());
-  if (getPageHeight() - getPageTop() < PAGE_CONFIGS.pageOffset) {
-    console.log("Almost end");
+  if (
+    getPageHeight() - getPageTop() < PAGE_CONFIGS.pageOffset &&
+    isMoreDataAvilable()
+  ) {
+    component(++state.currentPage);
   }
 }
 
-document.getElementById("main-content").appendChild(component());
+function _init() {
+  component(state.currentPage);
+  window.onscroll = _.debounce(infiteScroll, 100);
+}
 
-window.onscroll = _.debounce(infiteScroll, 100);
+_init();
